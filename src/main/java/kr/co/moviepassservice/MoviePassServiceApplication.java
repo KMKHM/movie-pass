6b38b89950e0f155d2acede2b33f3f883e0834f4
KMK_HM <kyun9151@gmail.com>
2025-03-05 21:19:50 +0900

    init project

---
+++ src/main/java/kr/co/moviepassservice/MoviePassServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.moviepassservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePassServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePassServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/moviepassservice/MoviePassServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.moviepassservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePassServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
