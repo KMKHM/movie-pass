3789975bce81ac74d1c995cef235a37725ef21f9
KMK_HM <kyun9151@gmail.com>
2025-04-08 14:43:06 +0900

    feat: 회원가입 작성

--- src/main/java/kr/co/moviepassservice/controller/member/MemberController.java
+++ src/main/java/kr/co/moviepassservice/controller/member/MemberController.java
@@ -1,11 +1,24 @@
 package kr.co.moviepassservice.controller.member;
 
+import kr.co.moviepassservice.dto.member.MemberDto;
+import kr.co.moviepassservice.dto.member.MemberDto.SignUpResponse;
 import kr.co.moviepassservice.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    // 회원가입
+    @PostMapping("/api/members")
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody MemberDto.SignUpRequest request) {
+        MemberDto.SignUpResponse response = memberService.signUp(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
